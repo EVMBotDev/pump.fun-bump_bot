54023526b37344308eb69541b695ef0642de8763
EVMBotDev <apexponto1@gmail.com>
2024-07-19 10:30:21 -0500

    Update index.js
    buy range implementation

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const RPC_URL = ""; // Quicknode or Helius give good rpc urls
 const PRIVKEY = ""; // the private key of the account who will buy and sell, in base58 (phantom export for example)
 const TOKEN_ADDR = ""; // Put the address of the token you want to bump here
 
-const SOL_BUY_AMOUNT = 0.011; // here you can choose to increase/decrease the buy amount
+const MIN_SOL_BUY_AMOUNT = 0.011; // here you set minimum amount of SOL per buy.
+const MAX_SOL_BUY_AMOUNT = 0.1; // here you set maximum amount of SOL per buy.
+
+const SOL_BUY_AMOUNT = Math.random() * (MAX_SOL_BUY_AMOUNT - MIN_SOL_BUY_AMOUNT) + MIN_SOL_BUY_AMOUNT;  // does math to give buy amount within specified range
 
 const FEES = 0.0005; // here you can adjust the fees
 const SLIPPAGE = 20; // here you can adjust the slippage
